cc3d6d0db1acb8fbd5bae297f65b13058976d1b7
Abdullah Ashouri <130899494+aashouri123@users.noreply.github.com>
2024-02-20 21:30:48 -0800

    Update Bank.java

--- BankProgram/Bank.java
+++ BankProgram/Bank.java
@@ -1,16 +1,13 @@
-import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class Bank {
 
-	public static int accountsneeded;
-
-	static ArrayList<Double> acctblc = new ArrayList<>();
-	static ArrayList<Integer> acctNums = new ArrayList<>();
-	static ArrayList<String> acctNam = new ArrayList<>();
+	public static Account[] accountsArray;
 
 	public static void main(String[] args) {
-		final int EXIT_ACTION = 0;
+
+		
 		final int REGISTER_ACTION = 1;
 		final int DEPOSTI_ACTION = 2;
 		final int WITHDRAW_ACTION = 3;
@@ -19,12 +16,152 @@ public class Bank {
 		final int DISPLAY_ALL_ACCOUNTS_ACTION = 6;
 		final int CALCULATE_A_LOAN_ACTION = 7;
 
-		boolean running = true;
+		int accounts = 10;
 
 		Scanner scan = new Scanner(System.in);
 
-		Account acct = new Account();
+		menu();
 
+		int input = scan.nextInt();
+
+		while (input != 0) {
+
+			switch (input) {
+
+			case REGISTER_ACTION:
+
+				System.out.println("Register an Account");
+				System.out.println("How many Accounts would you like to register: ");
+				accounts = scan.nextInt();
+
+				accountsArray = new Account[accounts];
+
+				for (int i = 0; i < accounts; i++) {
+
+					System.out.println("Enter Account Number: ");
+					int accountnumber = scan.nextInt();
+					System.out.println("Enter Account Name: ");
+					String name = scan.next();
+					System.out.println("Enter Account Balance: ");
+					double balance = scan.nextDouble();
+
+					accountsArray[i] = new Account(accountnumber, balance, name);
+
+					System.out.println("-------------------------------");
+					System.out.println("Account have been registered.");
+					System.out.println("-------------------------------");
+				}
+				break;
+
+			case DEPOSTI_ACTION: {
+
+				System.out.println("Enter account number to deposit: ");
+				int acctnum = scan.nextInt();
+				System.out.println("Enter amount to deposit: ");
+				double amount = scan.nextDouble();
+
+				for (int i = 0; i < accounts; i++) {
+					int acct = accountsArray[i].getaccountnumber();
+
+					if (acct == acctnum) {
+						accountsArray[i].deposit(amount);
+					}
+				} 
+
+				break;
+
+			}
+			case WITHDRAW_ACTION: {
+				
+				System.out.println("Enter account number to withdraw: ");
+				int acctnum = scan.nextInt();
+				System.out.println("Enter amount to withdraw: ");
+				double amount = scan.nextDouble();
+
+				for (int i = 0; i < accounts; i++) {
+					int acct = accountsArray[i].getaccountnumber();
+
+					if (acct == acctnum) {
+						System.out.println();
+						accountsArray[i].withdraw(amount);;
+					}
+				}
+				
+				break;
+
+			}
+			case TRANSFER_ACTION: {
+				
+				System.out.println("Enter amount to transfer: ");
+				double amount = scan.nextDouble();
+				System.out.println("Enter the sending account: ");
+				int fromAccount = scan.nextInt();
+				System.out.println("Enter the reciving account: ");
+				int toAccount = scan.nextInt();
+				
+				for (int i = 0; i < accounts; i++) {
+
+					if (fromAccount == accountsArray[i].getaccountnumber()) {
+						System.out.println();
+						accountsArray[i].withdraw(amount);;
+					}
+				}
+				
+				for (int i = 0; i < accounts; i++) {
+				
+					if (toAccount == accountsArray[i].getaccountnumber() ) {
+						System.out.println();
+						accountsArray[i].deposit(amount);
+					}
+				}
+				
+				break;
+
+			}
+			case DISPLAY_AN_ACCOUNT_ACTION: {
+				System.out.println("Enter account number to display account: ");
+				int accountnumber = scan.nextInt();
+				
+				for(int i = 0; i < accounts; i++) {
+					if (accountnumber == accountsArray[i].getaccountnumber()) {
+						System.out.println();
+						accountsArray[i].displayAccount();;
+					}
+				}
+
+				break;
+
+			}
+			case DISPLAY_ALL_ACCOUNTS_ACTION: {
+				
+				for(int i = 0; i < accounts; i++) {
+					
+					accountsArray[i].displayAccount();
+				}
+				break;
+
+			}
+			case CALCULATE_A_LOAN_ACTION: {
+				
+				break;
+
+			}
+			
+			default: {
+				System.out.println("Invalid input. Try again");
+				break;
+
+			}
+			}
+
+			menu_with_termination();
+			input = scan.nextInt();
+		}
+		System.out.println("Bank is Exited");
+
+	}
+
+	public static void menu() {
 		System.out.println("\t\tWelcome to Bank\n");
 		System.out.println("\tMenu\n");
 		System.out.println("1. Register.");
@@ -34,148 +171,19 @@ public class Bank {
 		System.out.println("5. Display An Account.");
 		System.out.println("6. Display All Accounts.");
 		System.out.println("7. Calculate a Loan.\n");
-
-		System.out.println("What would you like to do(Enter a number from the menu): ");
-		int input = scan.nextInt();
-
-		while (running) {
-
-			if (input == REGISTER_ACTION) {
-
-				System.out.println("How many accounts would you like to enter: ");
-				accountsneeded = scan.nextInt();
-
-				for (int i = 0; i < accountsneeded; i++) {
-					System.out
-							.println("Creat an accout Number Enter full name and balance:(Ex: 12345 Abdallah 500.0) ");
-					int accountNumber = scan.nextInt();
-					String name = scan.next();
-					double balance = scan.nextDouble();
-					register(accountNumber, name, balance);
-					System.out.println("Account is registered.");
-					}	
-
-			}
-
-			if (input == DEPOSTI_ACTION) {
-
-				System.out.println("Enter account Number to make a Deposit: ");
-				int accountnumber = scan.nextInt();
-				acct.getaccountNumber(accountnumber);
-
-				System.out.println("Enter amount to Deposit: ");
-				double amt = scan.nextDouble();
-
-				System.out.println("New Balance is: " + acct.deposit(accountnumber, amt));
-
-			}
-
-			if (input == WITHDRAW_ACTION) {
-
-				System.out.println("Enter account Number to make a Withdraw: ");
-				int accountnumber = scan.nextInt();
-				acct.getaccountNumber(accountnumber);
-
-				System.out.println("Enter amount to Withdraw: ");
-				double amt = scan.nextDouble();
-
-				System.out.println("New Balance is: " + acct.withdraw(accountnumber, amt));
-
-			}
-			
-			if(input == TRANSFER_ACTION) {
-				
-				System.out.println("Enter the Account Numbers.");
-				System.out.print("Account number to transfer from: ");
-				int acctNumFrom = scan.nextInt();
-				System.out.print("\nAccount number to transfer to: ");
-				int acctNumTo = scan.nextInt();
-				System.out.print("\nAmmount to transfer: ");
-				int amt = scan.nextInt();
-				
-				transfer(acctNumFrom, acctNumTo, amt);
-				
-				
-			}
-
-			if (input == DISPLAY_AN_ACCOUNT_ACTION) {
-				System.out.println("Enter Account number to be displayed: ");
-				int accountNumber = scan.nextInt();
-
-				acct.displayOneAcct(accountNumber);
-
-			}
-
-			if (input == DISPLAY_ALL_ACCOUNTS_ACTION) {
-
-				displayAllAccts();
-
-			}
-
-			if (input == CALCULATE_A_LOAN_ACTION) {
-
-			}
-
-			System.out.println("\n\n\t\tWelcome to Bank");
-			System.out.println("\tMenu\n");
-			System.out.println("1. Register.");
-			System.out.println("2. Deposit.");
-			System.out.println("3. Withdraw.");
-			System.out.println("4. Transfer.");
-			System.out.println("5. Display Account.");
-			System.out.println("6. Display All Accounts.");
-			System.out.println("7. Calculate a Loan.\n");
-
-			System.out.println("What would you like to do(Enter a number from the menu): \n" + "Enter 0 to exit");
-			input = scan.nextInt();
-
-			if (input == EXIT_ACTION) {
-				break;
-			}
-
-		}
-
+		System.out.println("Pick a a Service by the number: ");
 	}
 
-	public static void displayAllAccts() { // must be bank acct
-
-		for (int i = 0; i < accountsneeded; i++) {
-			System.out.println(i);
-			System.out.println("--------------------------------------");
-			System.out.println("Account Number: " + acctNums.get(i));
-			System.out.println("Name: " + acctNam.get(i));
-			System.out.println("Balance: " + acctblc.get(i));
-
-		}
-		System.out.println("--------------------------------------");
-	}
-
-	public static void register(int accountNumber, String name, double balance) { // must be in bank class
-
-		for (int i = 0; i < accountsneeded; i++) {
-			acctNums.add(accountNumber);
-			acctNam.add(name);
-			acctblc.add(balance);
-		}
-	}
-
-	public static void transfer(int accountNumberFrom, int accountNumberTo, int amt) {
-
-		int indexfrom = acctNums.indexOf(accountNumberFrom);
-		double balanceFrom = acctblc.get(indexfrom);
-		double newBalanceFrom = balanceFrom - amt;
-		acctblc.add(indexfrom, newBalanceFrom);
-
-		int indexTo = acctNums.indexOf(accountNumberTo);
-		double balanceTo = acctblc.get(indexTo);
-		double newbalanceTo = balanceTo + amt;
-		acctblc.add(indexTo, newbalanceTo);
-
-		System.out.println("Balance of " + accountNumberFrom + ": " + balanceFrom);
-		System.out.println("New balance: " + newBalanceFrom + "\n");
-		System.out.println("Balance of " + accountNumberTo + ": " + balanceTo);
-		System.out.println("New Balance: " + newbalanceTo + "\n");
-
+	public static void menu_with_termination() {
+		System.out.println("\tMenu\n");
+		System.out.println("1. Register.");
+		System.out.println("2. Deposit.");
+		System.out.println("3. Withdraw.");
+		System.out.println("4. Transfer.");
+		System.out.println("5. Display An Account.");
+		System.out.println("6. Display All Accounts.");
+		System.out.println("7. Calculate a Loan.\n");
+		System.out.println("Pick a a Service by the number. Enter 0 to exit: ");
 	}
 
 }
