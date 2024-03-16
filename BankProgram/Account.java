dea1fc0af75526607efc301dc14bc82d7526fcbf
Abdullah Ashouri <130899494+aashouri123@users.noreply.github.com>
2024-03-16 13:05:00 -0700

    Update Account.java

--- BankProgram/Account.java
+++ BankProgram/Account.java
@@ -4,60 +4,53 @@ public class Account {
 	int accountnumber;
 	double balance;
 	double amount;
-	
+
 	Account(int acctnum, double balance, String name) {
 		this.accountnumber = acctnum;
 		this.balance = balance;
 		this.name = name;
 	}
-	
+
 	void setaccountname(String name) {
 		this.name = name;
 	}
-	void setaccountnumber(int accountnumber) {
-		this.accountnumber = accountnumber;
-	}
-	void setbalance(double balance) {
-		this.balance = balance;
-	}
-	
+
 	int getaccountnumber() {
 		return accountnumber;
 	}
+
 	double getbalance() {
 		return balance;
 	}
+
 	String getname() {
 		return name;
 	}
-	
-	public void deposit(double amount){
-		
-		balance+=amount;
+
+	public void deposit(double amount) {
+
+		balance += amount;
 		System.out.println("Account Number: " + accountnumber);
 		System.out.println("Account Name: " + name);
-		System.out.println("New Balance: " + balance+" \n");
+		System.out.println("New Balance: " + balance + " \n");
 	}
-	
-	public void withdraw(double amount){
-		balance-=amount;
+
+	public void withdraw(double amount) {
+
+		balance -= amount;
 		System.out.println("Account Number: " + accountnumber);
 		System.out.println("Account Name: " + name);
-		System.out.println("New Balance: " + balance+" \n");
-		
-		
+		System.out.println("New Balance: " + balance + " \n");
 	}
-	
+
 	public void displayAccount() {
-		
+
 		System.out.println("------------------------------------");
 		System.out.println("Account Number: " + accountnumber);
-		System.out.println("Account Name: " + name );
+		System.out.println("Account Name: " + name);
 		System.out.println("Account Balance: " + balance);
 		System.out.println("------------------------------------");
 
-		
 	}
-	
-	
+
 }
