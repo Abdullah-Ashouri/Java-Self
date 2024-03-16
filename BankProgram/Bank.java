04733ff1dd40e32acecf1287b4ad1143d438f656
Abdullah Ashouri <130899494+aashouri123@users.noreply.github.com>
2024-03-16 13:04:27 -0700

    Update Bank.java

--- BankProgram/Bank.java
+++ BankProgram/Bank.java
@@ -112,43 +112,39 @@ public class Bank {
 			}
 
 			case CALCULATE_A_LOAN_ACTION: {
-				
-				
+
 				System.out.println("Enter your credit score: ");
 				int creditScore = scan.nextInt();
 				System.out.println("Enter loan amount: ");
 				double loanAmount = scan.nextDouble();
 				System.out.println("Enter loan term in months: ");
 				int monthsToPay = scan.nextInt();
+
 				
-				
-				double interestRate = get_Interest_Rate(creditScore);
-				
-				System.out.println("Interest Rate: " + interestRate);
-				
-				Loan loan = new Loan(loanAmount,monthsToPay,interestRate);
-				
-				double tenPercentOfTotals = total_balances_in_bank() * 0.1;
-				
-				if (tenPercentOfTotals > loanAmount) {
-					
-					System.out.println("Great we can offer you a loan.");
-					System.out.printf("Your monthly payment is: %.2f\n" , loan.interestRateCalculated());
-					
-				}else {
-					System.out.println("Unfortunatly we are unable to offer you a loan for that amount.");
+				System.out.println("Interest Rate: " + get_Interest_Rate(creditScore));
+
+				Loan loan = new Loan(loanAmount, monthsToPay, get_Interest_Rate(creditScore));
+
+		
+
+				if ((total_balances_in_bank() * 0.1) > loanAmount) {
+
+					System.out.println("Great we can offer you a loan.\n");
+					System.out.printf("Your monthly payment is: %.2f\n", loan.monthlyPaymentCalculated());
+
+				} else {
+					System.out.println("Unfortunatly we are unable to offer you a loan for that amount.\n");
 				}
-				
-				
+
 				break;
 
 			}
 
 			default: {
-				System.out.println("Invalid input. Try again");
+				System.out.println("Invalid input. Try again\n");
 				break;
 
-				}
+			}
 			}
 
 			menu_with_termination();
@@ -158,7 +154,7 @@ public class Bank {
 
 	}
 
-	public static void menu() {
+	public static void menu() {  // menu
 		System.out.println("\t\tWelcome to Bank\n");
 		System.out.println("\tMenu\n");
 		System.out.println("1. Register.");
@@ -171,7 +167,7 @@ public class Bank {
 		System.out.println("Pick a Service by the number: ");
 	}
 
-	public static void menu_with_termination() {
+	public static void menu_with_termination() {  // menu with termination
 		System.out.println("\tMenu\n");
 		System.out.println("1. Register.");
 		System.out.println("2. Deposit.");
@@ -183,7 +179,7 @@ public class Bank {
 		System.out.println("Pick a a Service by the number. Enter 0 to exit: ");
 	}
 
-	public static void search_with_deposit(int accounts, int acctnum, double amount) {
+	public static void search_with_deposit(int accounts, int acctnum, double amount) { // deposit function
 		for (int i = 0; i < accounts; i++) {
 			int acct = accountsArray[i].getaccountnumber();
 
@@ -193,7 +189,7 @@ public class Bank {
 		}
 	}
 
-	public static void search_with_withdraw(int accounts, int acctnum, double amount) {
+	public static void search_with_withdraw(int accounts, int acctnum, double amount) {  // withdraw funciton
 		for (int i = 0; i < accounts; i++) {
 			int acct = accountsArray[i].getaccountnumber();
 
@@ -205,7 +201,7 @@ public class Bank {
 		}
 	}
 
-	public static void display_account(int accounts, int accountnumber) {
+	public static void display_account(int accounts, int accountnumber) {  // displayes one account give an account number
 		for (int i = 0; i < accounts; i++) {
 			if (accountnumber == accountsArray[i].getaccountnumber()) {
 				System.out.println();
@@ -216,59 +212,58 @@ public class Bank {
 
 	}
 
-	public static void display_all_accounts(int accounts) {
+	public static void display_all_accounts(int accounts) {  // displayes all accounts info 
 		for (int i = 0; i < accounts; i++) {
 
 			accountsArray[i].displayAccount();
 		}
 	}
-	
-	public static double total_balances_in_bank() {
+
+	public static double total_balances_in_bank() {   // returns the total balances of all accounts
 		double totalBalance = 0;
-		
-		for(int i = 0; i < accountsArray.length; i++) {
+
+		for (int i = 0; i < accountsArray.length; i++) {
 			totalBalance += accountsArray[i].getbalance();
 		}
-		
+
 		return totalBalance;
-		
+
 	}
-	
-	public static double get_Interest_Rate(int creditScore) {
-		
+
+	public static double get_Interest_Rate(int creditScore) { // selects interest rate function based off credit score
+
 		double interestRate = 0;
-		
-		switch(creditScore/100) {
-			case 10:
-			case 9: {
-				interestRate = 3.50;
-				break;
-			}
-			case 8: {
-					interestRate = 4.00;
-				break;
-			}
-			case 7: {
-					interestRate = 4.50;
-				break;
-			}
-			case 6: {
-					interestRate = 5.00;
-				break;
-			}
-			case 5: 
-			case 4:
-			case 3:
-			case 2:
-			case 1: {
-				interestRate = 6.00;
-				break;
-			}
-			default: {
-				System.out.println("Not a valid Score");
-				break;
-			}
-			
+
+		switch (creditScore / 100) {
+		case 9: {
+			interestRate = 3.50;
+			break;
+		}
+		case 8: {
+			interestRate = 4.00;
+			break;
+		}
+		case 7: {
+			interestRate = 4.50;
+			break;
+		}
+		case 6: {
+			interestRate = 5.00;
+			break;
+		}
+		case 5:
+		case 4:
+		case 3:
+		case 2:
+		case 1: {
+			interestRate = 6.00;
+			break;
+		}
+		default: {
+			System.out.println("Not a valid Score");
+			break;
+		}
+
 		}
 		return interestRate;
 	}
